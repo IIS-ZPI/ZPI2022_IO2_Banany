18dd42a3cd0252b83b2915bcf524aca08de2d7ac
Alt-az <235979@edu.p.lodz.pl>
2023-03-08 21:38:13 +0100

    quick fix feature

--- lab1/Main.java
+++ lab1/Main.java
@@ -3,6 +3,6 @@ package lab1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("ZPI2022_IO2_Banany, Scrum Master Dominik Misztal");
-        System.out.println("Piotr Spętany");
+        System.out.println("Alt-az");
     }
 }
