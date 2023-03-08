03783d025c117e21a0d003d724886c7fd5ba33ac
KamilCiepluch <235845@edu.p.lodz.pl>
2023-03-08 18:58:27 +0100

    Add new feature

--- lab1/Main.java
+++ lab1/Main.java
@@ -2,6 +2,8 @@ package lab1;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("ZPI2022_IO2_Banany, Scrum Master Dominik Misztal");
+        System.out.println("KamilCiepluch");
     }
 }
