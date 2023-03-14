826886c38cebb38fdd172fac255eb2351362980b
Czapi12344 <236013@edu.p.lodz.pl>
2023-03-14 21:49:34 +0100

    Add multiplication
    add multiplication

--- lab1/Main.java
+++ lab1/Main.java
@@ -1,7 +1,17 @@
 package lab1;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ZPI2022_IO2_Banany. Scrum Master Dominik Misztal");
+
+        System.out.println("ZPI2022_IO2_Banany, Scrum Master Dominik Misztal");
+        System.out.println("Wanirur");
+        System.out.println("Alt-az");
+        System.out.println("KamilCiepluch");
+        System.out.println("Czapi12344");
+
+    }
+    public double Mult(double A,double B){
+        return A * B;
     }
 }
