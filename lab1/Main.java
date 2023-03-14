80135c2610a58b28c8be04764e380995a82f37dd
Wanirur <235861@edu.p.lodz.pl>
2023-03-14 17:36:31 +0100

    add method for subtracting 2 numbers

--- lab1/Main.java
+++ lab1/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("ZPI2022_IO2_Banany. Scrum Master Dominik Misztal");
     }
+
+    public static double difference(double a, double b) {
+        return a - b;
+    }
 }
