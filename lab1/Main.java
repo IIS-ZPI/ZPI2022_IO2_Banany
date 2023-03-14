e3ab59ead36446554febd9ff127ad84cbf60fddc
Alt-az <235979@edu.p.lodz.pl>
2023-03-14 10:56:13 +0100

    Addition method

--- lab1/Main.java
+++ lab1/Main.java
@@ -4,4 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("ZPI2022_IO2_Banany. Scrum Master Dominik Misztal");
     }
+    public double Addition(double A,double B){
+        return A+B;
+    }
 }
